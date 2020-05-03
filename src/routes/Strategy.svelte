fc02b81c9d45ceac3e57e1eb3b52756f2db9d2b6
Federico Varela <federicovr02@gmail.com>
2020-05-03 13:39:39 -0300

    Arreglos

--- src/routes/Strategy.svelte
+++ src/routes/Strategy.svelte
@@ -1,10 +1,35 @@
 <script>
-
     import {_} from "svelte-i18n";
+    import {langStore} from "../stores.js";
+    const products = JSON.parse(`{
+        "en": [
+            {"nombre": "Static sites",
+            "contenido": "These sites are simple "}
+        ],
+        "es": [
+
+        ]
+    }`)
 
 </script>
+<style>
+    h2 {
+        color: var(--text);
+        font-size: 120%;
+        margin-bottom: 10px;
+    }
+    ul {
+        list-style-type: none;
+        color: var(--text-secondary)
+    }
+</style>
 
 <h1>{$_("strategy.titulo")}</h1>
-<p>
-    {@html $_("strategy.contenido")}
-</p>
+<ul>
+    {#each products[$langStore] as product}
+         <li>
+            <h2>{product.nombre}</h2>
+            <p>{product.contenido}</p>
+         </li>
+    {/each}
+</ul>
